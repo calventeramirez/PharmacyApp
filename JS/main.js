a8b7e4ddc32d320a4407db0506faded4167654ae
Pablo Pérez Iza <perez.iza.pablo@gmail.com>
2024-05-03 15:06:01 +0200

    validacion registro

--- JS/main.js
+++ JS/main.js
@@ -17,22 +17,6 @@ boton.addEventListener("click", function (ev) {
         }
     }
     
-    //Validación contraseña
-    let contrasena = document.getElementsByName("contrasena")[0].value;
-    if (contrasena.length < 8 && formularioCorrecto || contrasena.length > 12 && formularioCorrecto) {
-        alert("La contraseña debe tener entre 8 y 12 caracteres");
-        formularioCorrecto = false;
-    }
-    
-    //Validacion email
-    let email = document.getElementsByName('email')[0].value;
-    let validacionEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    
-    if (validacionEmail.test(!email)) {
-        alert("El email es incorrecto")
-        formularioCorrecto = false;
-    } 
-    
     // Validación nombre
     let nombre = document.getElementsByName("nombre")[0].value.split(" ");
     if (document.getElementsByName("nombre")[0].value == "" && formularioCorrecto) {
@@ -58,37 +42,28 @@ boton.addEventListener("click", function (ev) {
         }
     }
     
-    //Validación telefono
-    let telefono = document.getElementsByName("telefono")[0].value;
-    if ((telefono.length) < 9 || (telefono.length) > 9) {
-            alert("Fallo, el telefono debe tener 9 digitos");
-            formularioCorrecto = false;
+    //Validacion email
+    let email = document.getElementsByName('email')[0].value;
+    let validacionEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    
+    if (validacionEmail.test(!email)) {
+        alert("El email es incorrecto")
+        formularioCorrecto = false;
+    } 
+    
+    //Validación contraseña
+    let contrasena = document.getElementsByName("contrasena")[0].value;
+    if (contrasena.length < 8 && formularioCorrecto || contrasena.length > 12 && formularioCorrecto) {
+        alert("La contraseña debe tener entre 8 y 12 caracteres");
+        formularioCorrecto = false;
     }
     
-    //validacion direccion
-    if (document.getElementsByName("direccion")[0].value == "" && formularioCorrecto) {
-        alert("La direccion no puede estar vacia")
-        formularioCorrecto = false;
+    //Validación repita contraseña
+    let contrasenaRep = document.getElementsByName("contrasenaRep")[0].value;
+    if( contrasenaRep != contrasena && formularioCorrecto){
+        alert("Las contraseñas deben ser la misma");
+        formularioCorrecto = false
     }
-
-    //Validación ciudad
-    let ciudad = document.getElementsByName("ciudad")[0].value.split(" ");
-    if (document.querySelectorAll("input")[2].value == "" && formularioCorrecto) {
-        alert("Elciudad no puede estar vacio")
-        formularioCorrecto = false;
-    } else {
-        if ((ciudad.length < 1 && formularioCorrecto) || (ciudad.length > 2 && formularioCorrecto)) {
-            alert("La ciudad deben tener una o dos palabras");
-            formularioCorrecto = false;
-        }
-    }
-    
-    //Validación codigo postal
-    let codigo_postal = document.getElementsByName("codigo_postal")[0].value;
-    if ((codigo_postal.length) < 5 || (codigo_postal.length) > 5) {
-        alert("Fallo, el codigo postal debe tener 5 digitos");
-        formularioCorrecto = false;
-    }  
 
     if(formularioCorrecto) location.href="index.thml";
 });
