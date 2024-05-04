4b09cfde6b3ecdb6049463abdaea510f832ffd3d
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-05-04 17:50:15 +0200

    Añadida la funcion para conectar a la bbdd

---
+++ funciones/conexion_bbdd.php
@@ -0,0 +1,9 @@
+<?php
+    $_servidor = "localhost"; //Para almacenar la ip de la base de datos
+    $_usuario = "admin";
+    $_contraseña = "admin";
+    $_base_de_datos = "db_pharmacyapp";
+
+    $conexion = new mysqli($_servidor, $_usuario, $_contraseña, $_base_de_datos)
+        or die("Error de conexión");
+?>
