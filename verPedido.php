56dbba63cad816e9847dc90723407e5f4fbcee34
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-05-25 04:02:02 +0200

    Añadido ver pedido

---
+++ verPedido.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PharmacyApp - Ver Pedido</title>
+    <link rel="icon" href="img/favicon.png" type="image/x-icon"> <!-- favicon -->
+    <link rel="stylesheet" href="./CSS/estilo.css"> <!-- Hoja de estilos -->
+    <link href="CSS/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap -->
+    <script src="./JS/api.js"></script> <!-- Script de JavaScript -->
+    <script src="JS/fontawesome.min.js" defer></script> <!-- Font Awesome -->
+</head>
+
+<body>
+    <?php session_start(); ?>
+    <header>
+        <div class="header-top">
+            <a href="/index.php" class="nav-logo"><img id="imagen-nav" src="img/logo_sin_fondo.png" alt="Logo"></a>
+            <div class="container">
+                <div class="buscador">
+                    <input type="text" placeholder="Buscar productos...">
+                </div>
+                <div class="cuenta-carrito">
+                    <?php
+                    // Si el usuario está logueado
+                    if (isset($_SESSION['usuario'])) {
+                        echo '<a href="./dashboard.php"><img src="/img/avatar.png" alt="Logo" class="icon icon-account" style="width: 30px; height: 30px; margin-right: 5px;"> ' . $_SESSION['usuario'] . '</a>';
+                        if ($_SESSION['rol'] == "admin") {
+                            echo '<a href="/anadir_medicamentos.php"><img src="/img/anadir-medicamento.png" alt="anadirmMedicamento" style="width: 30px; height: 30px; margin-right: 5px;">Añadir Medicamento</a>';
+                        }
+                        echo '<a href="/funciones/cerraSesion.php"><img src="/img/cerrar-sesion.png" alt="cerrarSesión" style="width: 25px; height: 20px; margin-right: 5px">Cerrar sesión</a>';
+                        echo '<a href="./carrito.php" ><img src="/img/carrito.png" alt="Carrito" style="width: 25px; height: 20px; margin-right: 5px">Carrito</a>';
+                    } else { // Si no está logueado
+                        echo '<a href="/login.php" class="cuenta"><img src="/img/iniciar-sesion.png" alt="iniciarsesion" style="width: 25px; height: 20px; margin-right: 5px">Iniciar Sesión</a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <nav>
+            <ul class="menu-verde">
+                <li><a href="/index.php">Inicio</a></li>
+                <li><a href="/medicamentos.php">Medicamentos</a></li>
+                <li><a href="/contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <!-- Sección de inicio -->
+        <?php
+            require "./funciones/conexion_bbdd.php";
+            $id_pedido = $_POST['idPedido'];
+            $sql ="SELECT * FROM lineaspedidos WHERE idPedido = '$id_pedido'";
+            $result = $conn->query($sql);
+        ?>
+        <h2>Tabla de productos del pedido <?php echo $id_pedido; ?></h2>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $precioTotal = 0.0;
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $sql2 = "SELECT nombre FROM medicamentos WHERE idMedicamento = " . $row['idMedicamento'];
+                            $result2 = $conn->query($sql2)->fetch_assoc()['nombre'];
+                            echo "<tr>";
+                            echo "<td>" . $result2 . "</td>";
+                            echo "<td>" . $row['cantidad'] . "</td>";
+                            echo "<td>" . $row['precioUnitario'] . "</td>";
+                            $precioTotal += $row['precioUnitario'];
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='3'>No hay productos en este pedido</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <h4>Precio Total: <?php echo $precioTotal; ?> </h4>
+            <a href="./pedidos.php"><button>Volver a pedidos</button></a>
+    </main>
+    <footer>
+        <div class="footer-container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <img src="img/logo_sin_fondo.png" alt="Logo" id="imagen-footer">
+                </div>
+                <div class="col-lg-7">
+                    <p> &copy; Desarrollado por: Pablo Jesús Calvente Ramírez, Fernando Dominguez Lago, Pablo Pérez Iza
+                        & Victor
+                        Moreno Benítez</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>Enlaces de interés</p>
+                    <ul>
+                        <li class="footer-li"><a href="/politicaPrivacidad.php">Política de privacidad</a></li>
+                        <li class="footer-li"><a href="/terminosCondiciones.php">Términos y condiciones</a></li>
+                        <li class="footer-li"><a href="/cookies.php">Política de cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="JS/bootstrap.bundle.min.js"></script> <!-- Bootstrap -->
+</body>
+
+</html>
